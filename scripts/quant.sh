f204e3b1034bf7643a06b328854cbbe5db70a519
Jennifer Westoby <jw28@sanger.ac.uk>
2019-05-23 10:02:10 +0100

    fixed for loop bug in quant.sh

--- scripts/quant.sh
+++ scripts/quant.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 input_arr=("$@")
-cd ../temp/sras
+cd ../temp/sras/sra
 
 for i in "${input_arr[@]}"
 do
   #wget url
-  ../software/sratoolkit.2.9.0-ubuntu64/bin/prefetch $i
+  ../../software/sratoolkit.2.9.0-ubuntu64/bin/prefetch $i
 
   #extract string from url
   filename=`echo $i`
 
-  cd sra
+  #cd sra
   #extract fastqs
    ../../software/sratoolkit.2.9.0-ubuntu64/bin/fastq-dump $filename".sra"
 
   #Kallisto quantification
-  ../../software/kallisto_linux-v0.43.1/kallisto -i ../index/transcripts.idx --threads=8 --output-dir=../results --single $filename".fastq" -l 200 -s 30
+  #../../software/kallisto_linux-v0.43.1/kallisto quant -i ../../index/transcripts.idx --threads=8 --output-dir=../../results/$filename -l 200 -s 30 --single $filename".fastq"
 
   #Delete everything except Kallisto results
   rm $filename*
